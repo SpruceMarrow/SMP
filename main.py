7e2ae905226652d917dd1a49abdf7003e9aff7ec
Nithin Kumar <sthestar1515@gmail.com>
2026-04-02 12:42:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -129,9 +129,9 @@ def helius():
                 for i in list1:
                     for j in i:
                         calist.append(j)
-                tick = data[0]['baseToken']['symbol']
-                fdv = data[0]['fdv']
-                amount = tx['nativeTransfers'][0]['amount'] / 1e9
+                tick = data['baseToken']['symbol']
+                fdv = data['fdv']
+                amount = tx['nativeTransfers']['amount'] / 1e9
                 print(f'{tick} {fdv} {amount}')
                 if fdv>80000 and amount>1:
                     print(f'{tick} {fdv} {amount}')
