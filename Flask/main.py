c49b839d5f1d5b50ddbddd2234d0c0ea4f446500
Nithin Kumar <157599772+SpruceMarrow@users.noreply.github.com>
2026-04-04 20:10:06 +0530

    Fix balance retrieval from database query result

--- Flask/main.py
+++ Flask/main.py
@@ -24,7 +24,7 @@ def getbal():
     cursor = sql.cursor()
     cursor.execute("SELECT Balance FROM bal")
     rec = list(cursor.fetchall())
-    bal = rec[0]
+    bal = rec[0][0]
     return bal
 
 
