5a63259f2c4718bae96baca4c627b4dc8914507a
Nithin Kumar <157599772+SpruceMarrow@users.noreply.github.com>
2026-04-08 09:39:56 +0530

    Add insert operation after delete in del.py
    Added an insert operation after deleting from the port table.

--- dbcmd/del.py
+++ dbcmd/del.py
@@ -5,6 +5,8 @@ sql = psycopg2.connect(os.environ["DATABASE_URL"])
 cur = sql.cursor()
 cur.execute("DELETE FROM port")
 print("Deleted")
+cur.execute("INSERT INTO bal values(1)")
+print("Executed")
 sql.commit()
 sql.close()
   
